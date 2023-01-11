524c899a67c7636fcc614dc31471321edcc80530
mherkhachatryan <mherkhachatryan35912@gmail.com>
2023-01-11 22:23:36 +0400

    neptune logger added to init, to not initialize new logger when calling it outside the code.

--- recon/__init__.py
+++ recon/__init__.py
@@ -10,7 +10,7 @@ warnings.filterwarnings("ignore")
 import model
 import preprocessing
 import configs
-from experiment_tracking import tb_writer
+from experiment_tracking import tb_writer, neptune_logger
 
 __all__ = [model, preprocessing,
-           tb_writer, configs]
+           tb_writer, configs, neptune_logger]
