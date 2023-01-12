4aec7b1c762797fb469cbeffa94c9e0066230936
mherkhachatryan <mherkhachatryan35912@gmail.com>
2023-01-12 18:05:41 +0400

    new experiment

--- recon/configs.py
+++ recon/configs.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 # path setting
-experiment_name = "depth_5_resnet50_thresh_0546"
+experiment_name = "depth_5_resnet152_thresh_0546"
 data_path = Path("/content/drive/MyDrive/recon_training/data_light")
 log_path = Path("/content/drive/MyDrive/recon_training/logs/")
 os.makedirs(log_path, exist_ok=True)
@@ -15,13 +15,13 @@ OUTPUT_SHAPE = (512, 512)  # do not change for now
 
 # training parameters
 BATCH_SIZE = 64
-EPOCHS = 80
+EPOCHS = 120
 VALID_SIZE = 0.2
 LOSS = "BCELoss"  # do not change for now
 OPTIMIZER = "adam"  # do not change for now
 SEGMENTATION_THRESHOLD = 0.546
 
 # MODEL PARAMETERS
-MODEL_NAME = "resnet50"
+MODEL_NAME = "resnet152"
 ENCODER_DEPTH = 5
 DECODER_CHANNELS = [256, 128, 64, 32, 16]
